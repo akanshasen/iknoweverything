5f71f69aa63c516178b9d120f5b204307f31b5de
Akansha Sen <akanshasen@hotmail.com>
2021-11-09 17:32:08 +1300

    started on iknoweverything project

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(IKnoweverythingWebApp());
+}
+
+class IKnoweverythingWebApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp();
+  }
+}
