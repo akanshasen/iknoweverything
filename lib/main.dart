af6c68de4c7fc46f04e8fab7f94bcd2e4c87d23f
Akansha Sen <akanshasen@hotmail.com>
2021-11-10 14:49:50 +1300

    added blank question answer page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iknoweverything/screens/question_answer.dart';
 
 void main() {
   runApp(IKnoweverythingWebApp());
@@ -8,6 +9,10 @@ class IKnoweverythingWebApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'I Know Everything',
+      home: QuestionAnswerPage(),
+    );
   }
 }
