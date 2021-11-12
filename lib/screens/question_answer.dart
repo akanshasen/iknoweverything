4525abb2d17df490b489904ae065d795b9cad9cd
Akansha Sen <akanshasen@hotmail.com>
2021-11-12 17:42:06 +1300

    got the api request working. Has an error

--- lib/screens/question_answer.dart
+++ lib/screens/question_answer.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:iknoweverything/models/answer.dart';
 
 class QuestionAnswerPage extends StatefulWidget {
   const QuestionAnswerPage({Key? key}) : super(key: key);
@@ -12,11 +15,24 @@ class _QuestionAnswerPageState extends State<QuestionAnswerPage> {
   /// Text editing controller for question text field
   TextEditingController _questionFieldController = TextEditingController();
 
+  /// To store the current asnwer object
+  late Answer _currentAnswer;
+
   /// Function - to handle the process of getting a yes or no answer
   _handleGetAnswer() async {
     try {
-      http.Response response =
-          await http.get(Uri.parse('https://yesno.wtf/api'));
+      http.Response response = await http.get(
+        Uri.parse('https://yesno.wtf/api'),
+      );
+      if (response.statusCode == 200 && response.body != null) {
+        print(response.body.runtimeType);
+        Map<String, dynamic> responseBody = json
+            .decode(response.body); //dart convert lib, does the json decoding
+        Answer answer = Answer.fromMap(responseBody);
+        setState(() {
+          _currentAnswer = answer;
+        });
+      }
     } catch (err, stacktrace) {
       print(err);
       print(stacktrace);
@@ -52,12 +68,29 @@ class _QuestionAnswerPageState extends State<QuestionAnswerPage> {
                         RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(5.0),
                 ))),
-                onPressed: () {},
+                onPressed: _handleGetAnswer,
                 child: Text('Get Answer'),
               ),
               SizedBox(
                 width: 20,
               ),
+
+              //TODO: resolve the error
+              //
+              if (_currentAnswer != null)
+                Container(
+                  height: 250,
+                  width: 400,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      image: DecorationImage(
+                        image: NetworkImage(_currentAnswer.image),
+                      )),
+                ),
+              if (_currentAnswer != null)
+                SizedBox(
+                  width: 20,
+                ),
               ElevatedButton(
                 style: ButtonStyle(
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
